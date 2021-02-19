774c29a565af6a8ad3c6fa28397506d1587c4135
Cody Regis <codyregis6891@yahoo.com>
2021-02-18 22:33:31 -0800

    added readme, updated js

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # baseball-quiz
+
+For this assignment we were supposed to make a timed quiz.  
+
+I struggled with combining the timer aspect and the correct/inccorect answers. I was not able to get a clean operation. 
+
+I felt like I was close and could have figured it out if I had more time, but I was unable to do before the deadline. 
+
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,3 @@
-var currentQuestion = 0;
-var initialsInput = $("#initials-text");
-var initialsArray = [];
-var secondsLeft = (questions.length) * 10;
-var xInterval = null;
-var newInitials = null;
-
 var questions = [
     {
         title: "How many players are on defense at one time?",
@@ -37,4 +30,124 @@ var questions = [
     },
 ];
 
+var currentQuestion = 0;
+var initialsInput = $("#initials-text");
+var initialsArray = [];
+var secondsLeft = (questions.length) * 10;
+var xInterval = null;
+var newInitials = null;
 
+$(".start").click(function () {
+    setTimer();
+    $(".start").hide();
+    $(".quiz").show();
+    showQuestion();
+});
+
+function setTimer() {
+    xInterval = setInterval(function() {
+        $('#timer').html(secondsLeft);
+        secondsLeft--;
+        if (secondsLeft <= 0) {
+            clearInterval(xInterval);
+            timeUp();
+        }
+    }, 1000);
+}
+
+function showQuestion() {
+    var options = questions[currentQuestion].options;
+    var question = questions[currentQuestion].title;
+    $(".quiz h2").text(question);
+    $(".quiz ul").html("");
+    for (var i = 0; i < parseInt(options.length); i++) {
+        var show = questions[currentQuestion].options[i];
+        $(".quiz ul").append("<li>").append(show);
+    }
+
+    $("li").click(function() {
+        var guessid = $(this).attr("id");
+        var guess = questions[currentQuestion].options[guessid];
+        var answer = questions[currentQuestion].answer;
+
+        if (answer === guess) {
+            currentQuestion++;
+            showScorePage();
+        } else {
+            secondsLeft = secondsLeft - 10;
+            currentQuestion++;
+            showScorePage();
+        }
+    });
+}
+
+function showScorePage() {
+    if (currentQuestion < questions.length) {
+        showQuestion();
+    } else {
+        $("#timer").remove();
+        $(".quiz").hide();
+        $(".scoreContainer").append(secondsLeft);
+        $("#scoreNumber").append(secondsLeft);
+        clearInterval(xInterval);
+    }
+}
+
+function timeUp() {
+    $("#timer").text("Times Up!");
+    $(".quiz").hide();
+    $(".scoreContainer").show();
+    $("#scoreNumber").append(secondsLeft);
+    clearInterval(xInterval);
+}
+
+$("#submit-initials").click(function(e) {
+    e.preventDefault();
+    loadScores();
+    saveScores();
+    showScores();
+    highScoresPage();
+});
+
+function saveScores() {
+    var scoreName = initialsInput.val();
+    var highScores = scoreName + " : " + secondsLeft;
+    initialsArray.push(highScores);
+    initialsArray.sort(function(a, b){return b-a});
+    localStorage.setItem("listOfItems", JSON.stringify(initialsArray));
+}
+
+function loadScores() {
+    var savedScores = localStorage.getItem("listOfItems");
+    var allScores = JSON.parse(savedScores);
+
+    if (allScores != null) {
+        initialsArray = allScores;
+    }
+}
+
+function showScores() {
+    for (i = 0; i < initialsArray.length; i++) {
+        newInitials = $("<li></li>").append(initialsArray[i]);
+        $("#scoreList").append(newInitials);
+    }
+}
+
+function highScoresPage() {
+    $(".highScorePage").show();
+    $("#initialsArray").hide();
+}
+
+$("#view-highscores").click(function() {
+    $(".highScorePage").toggle();
+});
+
+$("#go-back").click(function() {
+    window.location.reload();
+});
+
+$("#clear").click(function(e) {
+    e.preventDefault();
+    $("#scoreList").css("display", "none");
+    localStorage.clear();
+});
